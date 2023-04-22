2c6457295ae4390b93d89f100031eb958a64fc5f
M4ic0n-Mir4nda <107010440+M4ic0n-Mir4nda@users.noreply.github.com>
2023-04-22 01:02:22 -0300

    Update

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = require("./routes/index")
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 routes(app)
 
